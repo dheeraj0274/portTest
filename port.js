2b8dd63ffdda3295bd20214ce91a69bdeb13fa4a
dheeraj0274 <136884023+dheeraj0274@users.noreply.github.com>
2024-08-14 03:47:52 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,27 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  var typed = new Typed(".typing", {
+    strings: [
+      "Web Developer",
+      "App Developer",
+      "Chai lover"
+    
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+});
+
+
+
+
+window.addEventListener('scroll' ,()=>{
+    var header = document.querySelector(".navbar");
+    header.classList.toggle("sticky" , window.scrollY > 0);
+});
+
+
+
+
+c
